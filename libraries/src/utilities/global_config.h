c216c8449b9bc4a9bca2882f9d9754b908f95551
FeiZhu <aangel.piggy@gmail.com>
2013-10-07 10:42:20 -0700

    minor modification

---
+++ libraries/src/utilities/global_config.h
@@ -0,0 +1,25 @@
+/*
+ * @file global_config.h 
+ * @brief This file is used to globally configurate Physika.
+ * @author FeiZhu
+ * 
+ * This file is part of Physika, a versatile physics simulation library.
+ * Copyright (C) 2013 Physika Group.
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public License v2.0. 
+ * If a copy of the GPL was not distributed with this file, you can obtain one at:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+#ifndef PHYSIKA_UTILITIES_GLOBAL_CONFIG_H_
+#define PHYSIKA_UTILITIES_GLOBAL_CONFIG_H_
+
+namespace Physika{
+
+#define PHYSIKA_USE_EIGEN_MATRIX
+#define PHYSIKA_USE_EIGEN_VECTOR
+
+}
+
+#endif //PHYSIKA_UTILITIES_GLOBAL_CONFIG_H_
---
+++ libraries/src/utilities/math_constants.h
@@ -0,0 +1,24 @@
+/*
+ * @file math_constants.h 
+ * @brief This file is used to define math constants frequently used in Physika.
+ * @author FeiZhu
+ * 
+ * This file is part of Physika, a versatile physics simulation library.
+ * Copyright (C) 2013 Physika Group.
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public License v2.0. 
+ * If a copy of the GPL was not distributed with this file, you can obtain one at:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+#ifndef PHYSIKA_UTILITIES_MATH_CONSTANTS_H_
+#define PHYSIKA_UTILITIES_MATH_CONSTANTS_H_
+
+namespace Physika{
+
+  const double PI = 3.141592653589793;
+
+}
+
+#endif //PHYSIKA_UTILITIES_MATH_CONSTANTS_H_
