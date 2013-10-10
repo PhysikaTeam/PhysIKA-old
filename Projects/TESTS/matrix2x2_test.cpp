94c17b29dd1b08f693fec2634d7efe3710aebdaf
FeiZhu <aangel.piggy@gmail.com>
2013-10-09 21:59:37 -0700

    fix annoying naming

---
+++ Projects/TESTS/matrix2x2_test.cpp
@@ -0,0 +1,23 @@
+/*
+ * @file matrix2x2_test.cpp 
+ * @brief Test the Matrix2x2 class.
+ * @author Fei Zhu
+ * 
+ * This file is part of Physika, a versatile physics simulation library.
+ * Copyright (C) 2013 Physika Group.
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public License v2.0. 
+ * If a copy of the GPL was not distributed with this file, you can obtain one at:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+#include <iostream>
+#include "Physika_Core/Matrices/matrix_2x2.h"
+using namespace std;
+
+int main()
+{
+  cout<<"Matrix2x2 Test"<<endl;
+  return 0;
+}
---
+++ Projects/WCSPH/README.md
@@ -0,0 +1,12 @@
+**WCSPH**
+
+Weakly compressible SPH fluid.
+
+@author:
+Sheng Yang
+
+@Last update of this file:
+Fei Zhu, 10/03/2013
+=============================================================
+
+**Alert: Don't forget to modify this file after your implementation!**
