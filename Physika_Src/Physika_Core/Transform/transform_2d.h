496d5e790919965a116840c74b37d9d1c4b3bfec
Vovery <symovery@gmail.com>
2014-07-17 22:17:12 +0800

    add transform2d, if you use transform<scale>before please change it to transform<scale, 3> and add head file

---
+++ Physika_Src/Physika_Core/Transform/transform_2d.h
@@ -0,0 +1,110 @@
+/*
+ * @file transform_2d.h 
+ * @brief transform class, brief class representing a rigid 2d transform use matrix and vector
+ * @author Sheng Yang
+ * 
+ * This file is part of Physika, a versatile physics simulation library.
+ * Copyright (C) 2013 Physika Group.
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public License v2.0. 
+ * If a copy of the GPL was not distributed with this file, you can obtain one at:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+#ifndef PHSYIKA_CORE_TRANSFORM_TRANSFORM_2D_H_
+#define PHSYIKA_CORE_TRANSFORM_TRANSFORM_2D_H_
+
+#include "Physika_Core/Utilities/physika_assert.h"
+#include "Physika_Core/Utilities/type_utilities.h"
+#include "Physika_Core/Vectors/vector_3d.h"
+#include "Physika_Core/Quaternion/quaternion.h"
+#include "Physika_Core/Matrices/matrix_2x2.h"
+#include "Physika_Core/Matrices/matrix_3x3.h"
+#include "Physika_Core/Transform/transform.h"
+
+
+namespace Physika{
+
+/*
+ * Transform is defined for float and double
+ */
+
+template <typename Scalar>
+class Transform<Scalar, 2>: public TransformBase
+{
+public:
+    /* Constructions */
+
+    Transform();
+	Transform(const Vector<Scalar, 2> &translation);
+	Transform(const Scalar &rotate_angle);
+	Transform(const SquareMatrix<Scalar, 2> &rotation);	   //rotation is represented by a matrix;
+	Transform(const Vector<Scalar, 2> &translation, const SquareMatrix<Scalar, 2> &rotation);
+	Transform(const SquareMatrix<Scalar, 2> &rotation, const Vector<Scalar, 2> &translation);
+	Transform(const Vector<Scalar, 2> &translation, const Scalar &rotate_angle);
+	Transform(const Scalar &rotate_angle, const Vector<Scalar, 2> &translation);
+	Transform(const SquareMatrix<Scalar, 2> &rotation, const Vector<Scalar, 2> &translation, const Vector<Scalar, 2> &scale);
+	Transform(const Vector<Scalar, 2> &translation, const SquareMatrix<Scalar, 2> &rotation, const Vector<Scalar, 2> &scale);
+	Transform(const Vector<Scalar, 2> &translation, const Vector<Scalar, 2> &scale,  const SquareMatrix<Scalar, 2> &rotation);
+	Transform(const Scalar &rotate_angle, const Vector<Scalar, 2> &translation, const Vector<Scalar, 2> &scale);
+	Transform(const Vector<Scalar, 2> &translation, const Scalar &rotate_angle, const Vector<Scalar, 2> &scale);
+	Transform(const Vector<Scalar, 2> &translation, const Vector<Scalar, 2> &scale,  const Scalar &rotate_angle);
+
+	Transform(const SquareMatrix<Scalar, 3> &matrix);   //Init transform from a matrix.now it's now all right.Suggest not use this construction.
+	Transform(const Transform<Scalar, 2> &);
+
+	/* Operators */
+	Transform<Scalar, 2> & operator = (const Transform<Scalar, 2> &);
+	bool operator == (const Transform<Scalar, 2> &);
+	bool operator != (const Transform<Scalar, 2> &);
+
+    /* Get and Set */
+
+	//Get Matrix
+	SquareMatrix<Scalar, 2> rotation2x2Matrix() const;
+	SquareMatrix<Scalar, 3> rotation3x3Matrix() const;
+	SquareMatrix<Scalar, 3> transform3x3Matrix() const;
+	SquareMatrix<Scalar, 3> scale3x3Matrix() const;
+	SquareMatrix<Scalar, 3> transformMatrix() const;
+
+	//Get Variable
+	inline Scalar rotateAngle() const { return this->rotate_angle_; }
+	inline Vector<Scalar, 2> translation() const { return this->translation_; }
+	inline Vector<Scalar, 2> scale() const { return this->scale_; }
+	
+	//Set
+	inline void setTranslation(const Vector<Scalar, 2> &translation) { this->translation_ = translation; }
+	inline void setRotationAngle(const Scalar &rotate_angle) { this->rotate_angle_ = rotate_angle; }
+	inline void setScale(const Vector<Scalar, 2> &scale) { this->scale_ = scale; }
+	inline void setRotation(const SquareMatrix<Scalar, 2> &rotation) { this->rotate_angle_ = acos(rotation(0,0));} 
+	inline void setRotation(const SquareMatrix<Scalar, 3> &rotation) { this->rotate_angle_ = acos(rotation(0,0));}
+	inline void setIdentity() { this->rotate_angle_ = 0; this->translation_ = Vector<Scalar, 2>(0); this->scale_ = Vector<Scalar, 2>(1);}
+
+	/* Functions */
+	Vector<Scalar, 2> transform(const Vector<Scalar, 2> &input) const;
+
+	Vector<Scalar, 2> rotate(const Vector<Scalar, 2> &input) const;	 //there is no class writed for rotate2D, if you want rotate, use this. or use matrix yourself!
+	Vector<Scalar, 2> translate(const Vector<Scalar, 2> &input) const; 
+	Vector<Scalar, 2> scaling(const Vector<Scalar, 2> &input) const;
+
+    static inline Transform<Scalar, 2> identityTransform() { return Transform<Scalar, 2>(); }
+
+protected:
+	//A rotate_angle can represent the rotation in 2D simplely
+	//anticlockwise is + or it is -;
+	//Use radian
+    Scalar rotate_angle_;
+    Vector<Scalar,2> translation_;
+    Vector<Scalar,2> scale_;
+
+protected:
+    PHYSIKA_STATIC_ASSERT((is_same<Scalar,float>::value||is_same<Scalar,double>::value),
+                           "Transform<Scalar> are only defined for Scalar type of float and double");
+};
+
+
+
+}//end of namespace Physika
+
+#endif //PHSYIKA_CORE_TRANSFORM_TRANSFORM_2D_H_
