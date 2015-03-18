99ded8364d68102a583edc93c36140257096714b
FeiZhu <aangel.piggy@gmail.com>
2015-03-18 14:55:21 +0800

    1. tested on ubuntu, fix linking error (explicitly linking x11)

--- Physika_Src/Physika_Dependency/freeglut/src/gles_stubs.c
+++ Physika_Src/Physika_Dependency/freeglut/src/gles_stubs.c
@@ -4,17 +4,17 @@
 #include "fg_internal.h"
 
 void fgDeactivateMenu( SFG_Window *window ) {
-  fprintf(stderr, "fgDeactivateMenu: STUB\n");
+  /*fprintf(stderr, "fgDeactivateMenu: STUB\n");*/
 }
 void fgDisplayMenu( void ) {
-  fprintf(stderr, "fgDisplayMenu: STUB\n");
+  /*fprintf(stderr, "fgDisplayMenu: STUB\n");*/
 }
 void fgUpdateMenuHighlight ( SFG_Menu *menu ) {
-  fprintf(stderr, "fgUpdateMenuHighlight: STUB\n");
+  /*fprintf(stderr, "fgUpdateMenuHighlight: STUB\n");*/
 }
 GLboolean fgCheckActiveMenu ( SFG_Window *window, int button, GLboolean pressed,
                               int mouse_x, int mouse_y )  {
-  fprintf(stderr, "fgCheckActiveMenu: STUB\n");
+  /*fprintf(stderr, "fgCheckActiveMenu: STUB\n");*/
   return GL_FALSE;
 }
 
