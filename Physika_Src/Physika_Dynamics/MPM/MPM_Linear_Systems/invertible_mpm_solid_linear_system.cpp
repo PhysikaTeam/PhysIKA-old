493b9b203e3e1b3507ecf63dcfb3ce4778c465ed
FeiZhu <aangel.piggy@gmail.com>
2015-11-12 08:23:39 +0800

    implementing linear system for invertible mpm solid

--- Physika_Src/Physika_Dynamics/MPM/MPM_Linear_Systems/invertible_mpm_solid_linear_system.cpp
+++ Physika_Src/Physika_Dynamics/MPM/MPM_Linear_Systems/invertible_mpm_solid_linear_system.cpp
@@ -12,8 +12,73 @@
 *
 */
 
+#include <typeinfo>
+#include "Physika_Core/Utilities/physika_exception.h"
+#include "Physika_Dynamics/MPM/invertible_mpm_solid.h"
+#include "Physika_Dynamics/MPM/MPM_Linear_Systems/enriched_mpm_uniform_grid_generalized_vector.h"
 #include "Physika_Dynamics/MPM/MPM_Linear_Systems/invertible_mpm_solid_linear_system.h"
 
 namespace Physika{
+
+template <typename Scalar, int Dim>
+InvertibleMPMSolidLinearSystem<Scalar, Dim>::InvertibleMPMSolidLinearSystem(InvertibleMPMSolid<Scalar, Dim> *invertible_driver)
+    :invertible_mpm_solid_driver_(invertible_driver), active_obj_idx_(-1)
+{
+
+}
+
+template <typename Scalar, int Dim>
+InvertibleMPMSolidLinearSystem<Scalar, Dim>::~InvertibleMPMSolidLinearSystem()
+{
+
+}
+
+template <typename Scalar, int Dim>
+void InvertibleMPMSolidLinearSystem<Scalar, Dim>::multiply(const GeneralizedVector<Scalar> &x, GeneralizedVector<Scalar> &result) const
+{
+    //TO DO
+}
+
+template <typename Scalar, int Dim>
+void InvertibleMPMSolidLinearSystem<Scalar, Dim>::preconditionerMultiply(const GeneralizedVector<Scalar> &x, GeneralizedVector<Scalar> &result) const
+{
+    //TO DO
+}
+
+template <typename Scalar, int Dim>
+void InvertibleMPMSolidLinearSystem<Scalar, Dim>::setActiveObject(int obj_idx)
+{
+    active_obj_idx_ = obj_idx;
+    //all negative values are set to -1
+    active_obj_idx_ = active_obj_idx_ < 0 ? -1 : active_obj_idx_;
+}
+
+template <typename Scalar, int Dim>
+void InvertibleMPMSolidLinearSystem<Scalar, Dim>::energyHessianMultiply(const EnrichedMPMUniformGridGeneralizedVector<Vector<Scalar, Dim> > &x_diff,
+                                                                        EnrichedMPMUniformGridGeneralizedVector<Vector<Scalar, Dim> > &result) const
+{
+    //TO DO
+}
+
+template <typename Scalar, int Dim>
+void InvertibleMPMSolidLinearSystem<Scalar, Dim>::jacobiPreconditionerMultiply(const EnrichedMPMUniformGridGeneralizedVector<Vector<Scalar, Dim> > &x,
+                                                                               EnrichedMPMUniformGridGeneralizedVector<Vector<Scalar, Dim> > &result) const
+{
+    //if (active_obj_idx_ == -1) //all objects solved together
+    //{
+    //    //TO DO
+    //}
+    //else  //solve for one active object
+    //{
+    //    //TO DO
+    //}
+    throw PhysikaException("Not implemented!");
+}
+
+//explicit instantiations
+template class InvertibleMPMSolidLinearSystem<float, 2>;
+template class InvertibleMPMSolidLinearSystem<float, 3>;
+template class InvertibleMPMSolidLinearSystem<double, 2>;
+template class InvertibleMPMSolidLinearSystem<double, 3>;
 
 }  //end of namespace Physika
--- Physika_Src/Physika_Dynamics/MPM/MPM_Linear_Systems/invertible_mpm_solid_linear_system.h
+++ Physika_Src/Physika_Dynamics/MPM/MPM_Linear_Systems/invertible_mpm_solid_linear_system.h
@@ -15,7 +15,43 @@
 #ifndef PHYSIKA_DYNAMICS_MPM_MPM_LINEAR_SYSTEMS_INVERTIBLE_MPM_SOLID_LINEAR_SYSTEM_H_
 #define PHYSIKA_DYNAMICS_MPM_MPM_LINEAR_SYSTEMS_INVERTIBLE_MPM_SOLID_LINEAR_SYSTEM_H_
 
+#include "Physika_Numerics/Linear_System_Solvers/linear_system.h"
+
 namespace Physika{
+
+template <typename Scalar, int Dim> class InvertibleMPMSolid;
+template <typename Scalar> class EnrichedMPMUniformGridGeneralizedVector;
+
+/*
+ * InvertibleMPMSolidLinearSystem: linear system for implicit integration of InvertibleMPMSolid
+ * x and b are represented by EnrichedMPMUniformGridGeneralizedVector
+ */
+
+template <typename Scalar, int Dim>
+class InvertibleMPMSolidLinearSystem : public LinearSystem<Scalar>
+{
+public:
+    explicit InvertibleMPMSolidLinearSystem(InvertibleMPMSolid<Scalar, Dim> *invertible_driver);
+    virtual ~InvertibleMPMSolidLinearSystem();
+    virtual void multiply(const GeneralizedVector<Scalar> &x, GeneralizedVector<Scalar> &result) const;
+    virtual void preconditionerMultiply(const GeneralizedVector<Scalar> &x, GeneralizedVector<Scalar> &result) const;
+    //set the object to construct the linear system for (each object of InvertibleMPMSolid is solved independently)
+    //construct one global linear system for all objects if obj_idx is set to -1 (all objects are solved on one grid)
+    void setActiveObject(int obj_idx);
+protected:
+    //potential energy Hessian acted on an arbitrary increment x_diff
+    void energyHessianMultiply(const EnrichedMPMUniformGridGeneralizedVector<Vector<Scalar, Dim> > &x_diff,
+                               EnrichedMPMUniformGridGeneralizedVector<Vector<Scalar, Dim> > &result) const;
+    void jacobiPreconditionerMultiply(const EnrichedMPMUniformGridGeneralizedVector<Vector<Scalar, Dim> > &x,
+                                      EnrichedMPMUniformGridGeneralizedVector<Vector<Scalar, Dim> > &result) const;
+    //disable default copy
+    InvertibleMPMSolidLinearSystem();
+    InvertibleMPMSolidLinearSystem(const InvertibleMPMSolidLinearSystem<Scalar, Dim> &linear_system);
+    InvertibleMPMSolidLinearSystem<Scalar, Dim>& operator= (const InvertibleMPMSolidLinearSystem<Scalar, Dim> &linear_system);
+protected:
+    InvertibleMPMSolid<Scalar, Dim> *invertible_mpm_solid_driver_;
+    int active_obj_idx_;
+};
 
 }  //end of namespace Physika
 
