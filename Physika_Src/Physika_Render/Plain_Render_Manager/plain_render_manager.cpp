9a02e2d7d29f85036691f872d23b008a2000ce18
suitmyself <1100011608@pku.edu.cn>
2018-01-19 22:13:58 +0800

    feat: add plain render manager

---
+++ Physika_Src/Physika_Render/Plain_Render_Manager/plain_render_manager.cpp
@@ -0,0 +1,24 @@
+/*
+* @file plain_render_manager.cpp
+* @Basic class PlainRenderManager
+* @author Wei Chen
+*
+* This file is part of Physika, a versatile physics simulation library.
+* Copyright (C) 2013- Physika Group.
+*
+* This Source Code Form is subject to the terms of the GNU General Public License v2.0.
+* If a copy of the GPL was not distributed with this file, you can obtain one at:
+* http://www.gnu.org/licenses/gpl-2.0.html
+*
+*/
+
+#include "plain_render_manager.h"
+
+namespace Physika{
+
+void PalinRenderManager::render()
+{
+    render_scene_config_->renderAllTasks();
+}
+
+}
