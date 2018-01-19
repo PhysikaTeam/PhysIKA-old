22a2379a08bffc3f432f774e9eafa2dfe4ddb0fb
suitmyself <1100011608@pku.edu.cn>
2018-01-19 22:17:13 +0800

    feat: add render_task_base

---
+++ Physika_Src/Physika_Render/Render_Task_Base/render_task_base.cpp
@@ -0,0 +1,75 @@
+/*
+ * @file render_task_base.cpp
+ * @Basic render task, all other render task class inherit from this class.
+ * @author Wei Chen
+ * 
+ * This file is part of Physika, a versatile physics simulation library.
+ * Copyright (C) 2013- Physika Group.
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public License v2.0. 
+ * If a copy of the GPL was not distributed with this file, you can obtain one at:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+
+
+#include "Physika_Render/OpenGL_Primitives/glew_utilities.h"
+#include "Physika_Core/Transform/transform_3d.h"
+#include "Physika_Render/OpenGL_Shaders/shader.h"
+#include "Physika_Render/Global_Unifrom_Config/global_uniform_config.h"
+
+#include "render_task_base.h"
+
+namespace Physika{
+
+void RenderTaskBase::renderTask()
+{
+    if (enable_bind_shader_ == true)
+        shader_.bindAndConfigBasicUniforms();
+
+    //config global uniforms
+    GlobalUniformConfig & global_uniform_config = GlobalUniformConfig::getSingleton();
+    global_uniform_config.configGlobalUniforms();
+
+    //set model transform
+    const Matrix4f & model_trans = transform_.transformMatrix();
+    openGLSetCurBindShaderMat4("model_trans", model_trans);
+
+    renderTaskImpl();
+
+    if (enable_bind_shader_ == true)
+        shader_.unBind();
+}
+
+RenderTaskType RenderTaskBase::type() const
+{
+    return RenderTaskType::NORMAL_RENDER_TASK;
+}
+
+void RenderTaskBase::enableBindShader()
+{
+    enable_bind_shader_ = true;
+}
+
+void RenderTaskBase::disableBindShader()
+{
+    enable_bind_shader_ = false;
+}
+
+bool RenderTaskBase::isEnableBindShader() const
+{
+    return enable_bind_shader_;
+}
+
+const Transform3f & RenderTaskBase::transform()const
+{
+    return this->transform_;
+}
+
+void RenderTaskBase::setTransform(const Transform3f & transform)
+{
+    this->transform_ = transform;
+}
+
+}//end of namespace Physika
---
+++ Physika_Src/Physika_Render/Render_Task_Base/render_task_base.h
@@ -0,0 +1,55 @@
+/*
+ * @file render_task_base.h 
+ * @Basic render task, all other render task class inherit from this class.
+ * @author Wei Chen
+ * 
+ * This file is part of Physika, a versatile physics simulation library.
+ * Copyright (C) 2013- Physika Group.
+ *
+ * This Source Code Form is subject to the terms of the GNU General Public License v2.0. 
+ * If a copy of the GPL was not distributed with this file, you can obtain one at:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+#pragma  once
+
+#include "Physika_Render/OpenGL_Shaders/shader.h"
+#include "Physika_Core/Transform/transform_3d.h"
+
+namespace Physika{
+
+
+enum class RenderTaskType
+{
+    NORMAL_RENDER_TASK,
+    SCREEN_BASED_RENDER_TASK
+};
+
+
+class RenderTaskBase
+{
+public:
+    virtual ~RenderTaskBase() = default;
+
+    void renderTask();
+
+    virtual RenderTaskType type() const;  //default to RenderTaskType::NORMAL_RENDER_TASK
+
+    void enableBindShader();
+    void disableBindShader();
+    bool isEnableBindShader() const;
+
+    const Transform3f & transform()const;
+    void setTransform(const Transform3f & transform);
+
+private:
+    virtual void renderTaskImpl() = 0;
+
+protected:
+    bool enable_bind_shader_ = true; //hook to enable/disable bind shader
+    Shader shader_;
+    Transform3f transform_;
+};
+    
+}//end of namespace Physika
