36ee4a2a5c4d4461840b61e4f022d7784944045f
suitmyself <1100011608@pku.edu.cn>
2018-01-19 21:45:07 +0800

    feat: reimplement spot light

--- Physika_Src/Physika_Render/Lights/spot_light.h
+++ Physika_Src/Physika_Render/Lights/spot_light.h
@@ -1,6 +1,6 @@
 /*
  * @file spot_light.h 
- * @Brief a spot light class for OpenGL.
+ * @Brief class SpotLight
  * @author Wei Chen
  * 
  * This file is part of Physika, a versatile physics simulation library.
@@ -11,30 +11,59 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
  */
-#ifndef PHYSIKA_GUI_LIGHTS_SPOT_LIGHT_H_
-#define PHYSIKA_GUI_LIGHTS_SPOT_LIGHT_H_
 
-#include "Physika_Render/Lights/light.h"
+#pragma  once
+
+#include <glm/glm.hpp>
+
+#include "Physika_Core/Utilities/math_utilities.h"
+#include "point_light.h"
 
 namespace Physika{
 
-class SpotLight: public Light
+class SpotLight: public PointLight
 {
 public:
-    // construction and destruction
-    SpotLight();
-    explicit SpotLight(GLenum light_id);
-    virtual ~SpotLight();
-    template<typename Scalar> void setSpotDirection(const Vector<Scalar,3>& direction);
-    template<typename Scalar> Vector<Scalar,3> spotDirection() const;
-    template<typename Scalar> void setSpotExponent(Scalar exponent);
-    template<typename Scalar> Scalar spotExponent() const;
-    template<typename Scalar> void setSpotCutoff(Scalar cutoff);
-    template<typename Scalar> Scalar spotCutoff() const;
+    SpotLight() = default;
+    SpotLight(const Color4f & ambient_col, 
+              const Color4f & diffuse_col, 
+              const Color4f & specular_col,
+              const Vector3f & pos,
+              float constant_attenuation,
+              float linear_attenuation,
+              float quadratic_attenution,
+              const Vector3f & spot_direction,
+              float spot_exponent,
+              float spot_cutoff);
+
+    LightType type() const override;
+    void configToCurBindShader(const std::string & light_str) override;
+
+    glm::mat4 lightProjMatrix() const;
+    glm::mat4 lightViewMatrix() const;
+    glm::mat4 lightTransformMatrix() const;
+
+    //Note: we define spotDirection() as virtual since we would change its definition is FlashLight Class
+    void setSpotDirection(const Vector3f & direction); 
+    virtual const Vector3f spotDirection() const; //return by value
+
+    void setSpotExponent(float exponent);
+    float spotExponent() const;
+
+    void setSpotCutoff(float cutoff);            //in degrees
+    float spotCutoff() const;                    //in degrees
+
+    //Note: Both spot_exponent & spot_outer_cutoff are used to implement the smooth/soft spot light effect
+    void setSpotOuterCutoff(float outer_cutoff); //in degrees
+    float spotOuterCutoff() const;               //in degrees
+
+
+private:
+    Vector3f spot_direction_ = {0.0f, 0.0f, -1.0f};
+    float spot_exponent_ = 0.0f;
+    float spot_cutoff_ = 180.0f;     //in degrees
+    float spot_outer_cutoff_ = 0.0f; //in degrees, 0 means no using of outer_cutoff
 };
 
 } //end of namespace Physika
 
-#include "Physika_Render/Lights/spot_light-inl.h"
-
-#endif //PHYSIKA_RENDER_LIGHTS_SPOT_LIGHT_H_
