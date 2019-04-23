2088a802968292de28faa76a42c0cd16f0e09885
superboxer <32252653+superboxer@users.noreply.github.com>
2019-04-23 17:20:52 +0800

    Update loadinputfile.h

--- Physika_Src/Physika_Shallow_Water/Shallow_Water_IO/loadinputfile.h
+++ Physika_Src/Physika_Shallow_Water/Shallow_Water_IO/loadinputfile.h
@@ -1,6 +1,8 @@
+#include "Physika_Shallow_Water/Shallow_water_modle/ShallowWaterSolver.h"
+#include "Physika_Shallow_Water/Shallow_Water_Render/render.h"
 #include <fstream>
 #include <cassert>
 #include <vector>
 #include <string>
 void load_field_data(std::string const file_name, std::vector<double> &field, size_t &x_cells, size_t &y_cells);
-void run(string *height,string *surface,string *vx.string *vy);
+void run(std::string const height,std::string const surface,std::string const vx,std::string const vy);
