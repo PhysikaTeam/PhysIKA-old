5383e7bd0ee7a7cd3ee83b601f8d6d70306aa68c
superboxer <32252653+superboxer@users.noreply.github.com>
2019-04-23 16:43:18 +0800

    Create Field.cpp

---
+++ Physika_Src/Physika_Shallow_Water/Shallow_Water_meshs/Field.cpp
@@ -0,0 +1,81 @@
+#include "Field.h"
+#include <cassert>
+#include <fstream>
+#include <sstream>
+
+Field::Field(size_t x_cells, size_t y_cells) :
+	base(x_cells * y_cells, 0),
+	x_cells(x_cells),
+	y_cells(y_cells) {}
+
+void Field::resize(size_t x_cells, size_t y_cells) {
+
+	this->x_cells = x_cells;
+	this->y_cells = y_cells;
+
+	base.resize(x_cells * y_cells);
+}
+
+double& Field::operator()(size_t x, size_t y) {
+
+	assert((x < x_cells) && (y < y_cells));
+	return base[x * y_cells + y];
+}
+
+double Field::operator()(size_t x, size_t y) const {
+
+	assert((x < x_cells) && (y < y_cells));
+	return base[x * y_cells + y];
+}
+
+Field Field::operator+(Field const &field) const {
+
+	Field sum(*this);
+	sum += field;
+
+	return sum;
+}
+
+Field Field::operator-(Field const &field) const {
+
+	Field diff(*this);
+	diff -= field;
+
+	return diff;
+}
+
+Field& Field::operator+=(Field const &field) {
+	assert(x_cells == field.x_cells && y_cells == field.y_cells);
+
+	size_t size = field.base.size();
+	for (size_t i = 0; i < size; ++i) {
+		base[i] += field.base[i];
+	}
+
+	return *this;
+}
+
+Field &Field::operator-=(Field const &field) {
+	assert(x_cells == field.x_cells && y_cells == field.y_cells);
+
+	size_t size = field.base.size();
+	for (size_t i = 0; i < size; ++i) {
+		base[i] -= field.base[i];
+	}
+
+	return *this;
+}
+
+Field Field::operator*(double multiplier) const {
+
+	Field scaled(*this);
+	for (double &element : scaled.base) {
+		element *= multiplier;
+	}
+
+	return scaled;
+}
+
+const std::vector<double> &Field::getBase() const {
+	return base;
+}
