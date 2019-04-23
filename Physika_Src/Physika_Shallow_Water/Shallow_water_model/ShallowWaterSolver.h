78a72033f7d4f0d4054d0b8998f0826710f3b14f
superboxer <32252653+superboxer@users.noreply.github.com>
2019-04-23 16:45:59 +0800

    Create ShallowWaterSolver.h

---
+++ Physika_Src/Physika_Shallow_Water/Shallow_water_model/ShallowWaterSolver.h
@@ -0,0 +1,33 @@
+#include "Physika_Shallow_Water/Shallow_Water_meshs/Field.h"
+class ShallowWaterSolver {
+public:
+	ShallowWaterSolver(size_t x_cells, size_t y_cells, double time_step, double dx, double gravity = 9.8);
+	void initialize_water_height(const std::vector<double> &input);
+	void initialize_vx(const std::vector<double> &input);
+	void initialize_vy(const std::vector<double> &input);
+	void initialize_surface_level(const std::vector<double> &input);
+	void run(size_t iterations);
+	const Field &getWater_height() const;
+	const Field &getSurface_level() const;
+	double getTime_elapsed() const;
+	void output(size_t iteration);
+private:
+	void euler();
+	Field advect_height(double time_step) const;
+	Field advect_vx(double time_step) const;
+	Field advect_vy(double time_step) const;
+	void update_height(double time_step);
+	void update_vx(double time_step);
+	void update_vy(double time_step)
+	void apply_reflecting_boundary_conditions();
+	double dt;
+	double dx;
+	double time_elapsed;
+	size_t x_cells;
+	size_t y_cells;
+	Field water_height;
+	Field vx;
+	Field vy;
+	Field surface_level;
+	double const gravity;
+};
