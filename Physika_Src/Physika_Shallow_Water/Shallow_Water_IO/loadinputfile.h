5ef9390df1c4c65b0e5b84173a916896bb7ab363
superboxer <32252653+superboxer@users.noreply.github.com>
2019-04-23 17:13:17 +0800

    Create loadinputfile.h

---
+++ Physika_Src/Physika_Shallow_Water/Shallow_Water_IO/loadinputfile.h
@@ -0,0 +1,6 @@
+#include <fstream>
+#include <cassert>
+#include <vector>
+#include <string>
+void load_field_data(std::string const file_name, std::vector<double> &field, size_t &x_cells, size_t &y_cells);
+void run(string *height,string *surface,string *vx.string *vy);
