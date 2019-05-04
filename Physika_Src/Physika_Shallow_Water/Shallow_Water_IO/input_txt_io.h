23f375fb44d71c747078f0bdaf6fc235039849eb
superboxer <32252653+superboxer@users.noreply.github.com>
2019-05-04 15:47:04 +0800

    Create input_txt_io.h

---
+++ Physika_Src/Physika_Shallow_Water/Shallow_Water_IO/input_txt_io.h
@@ -0,0 +1,9 @@
+#include "Physika_Shallow_Water/Shallow_water_model/ShallowWaterSolver.h"
+//#include "Physika_Shallow_Water/Shallow_Water_Render/render.h"
+#include <fstream>
+#include <cassert>
+#include <vector>
+#include <string>
+namespace Physika {
+void load_field_data(std::string const file_name, std::vector<double> &field, size_t &x_cells, size_t &y_cells);
+}
