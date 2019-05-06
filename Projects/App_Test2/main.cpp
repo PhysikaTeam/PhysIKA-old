adea16fa6be9bcf7061a036c872263cc333873db
superboxer <32252653+superboxer@users.noreply.github.com>
2019-05-06 13:31:36 +0800

    Delete main.cpp

--- Projects/App_Test2/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Physika_Src/Physika_Shallow_Water/Shallow_Water_IO"
-void main(){
-    string s1,s2,s3,s4;
-    cin>>s1;
-    cin>>s2;
-    cin>>s3;
-    cin>>s4;
-    run(s1,s2,s3,s4);
-    }
