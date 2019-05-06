70dcedd46d1836a4e02dbe27ea2a733d4067cb7d
superboxer <32252653+superboxer@users.noreply.github.com>
2019-05-06 14:58:36 +0800

    Update showmesh.h

--- Physika_Src/Physika_Surface_Fuild/Surface_Render/showmesh.h
+++ Physika_Src/Physika_Surface_Fuild/Surface_Render/showmesh.h
@@ -1,9 +1,9 @@
-#ifndef SHOWMESH_H
-#define SHOWMESH_H
+#ifndef PHYSIKA_SURFACE_FUILD_SURFACE_RENDER_SHOWMESH_H_
+#define PHYSIKA_SURFACE_FUILD_SURFACE_RENDER_SHOWMESH_H_
 
 #include "Physika_Surface_Fuild/Surface_Triangle_Meshs/mymesh.h"
-
+namespace Physika{
 void show_mesh ( int argc, char *argv[]);
 void show_mesh(int argc, char *argv[], MyMesh const &mesh);
-
+}
 #endif
