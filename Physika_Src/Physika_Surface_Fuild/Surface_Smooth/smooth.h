652bd3acb786e34c620d1d267e5c6d3a54f72c65
superboxer <32252653+superboxer@users.noreply.github.com>
2019-05-06 14:55:58 +0800

    Update smooth.h

--- Physika_Src/Physika_Surface_Fuild/Surface_Smooth/smooth.h
+++ Physika_Src/Physika_Surface_Fuild/Surface_Smooth/smooth.h
@@ -1,10 +1,10 @@
-#ifndef SMOOTH_H
-#define SMOOTH_H
+#ifndef PHYSIKA_SURFACE_FUILD_SURFACE_SMOOTH_SMOOTH_H_
+#define PHYSIKA_SURFACE_FUILD_SURFACE_SMOOTH_SMOOTH_H_
 
 #include "Physika_Surface_Fuild/Surface_Triangle_Meshs/mymesh.h"
 #include <vector>
 #include <functional>
-
+namespace Physika{
 void jacobi_laplace_smooth(MyMesh &mesh, int smooth_times);
 
 void decimater(MyMesh &mesh, double max_err);
@@ -14,5 +14,5 @@ MyMesh marching_cubes(MyMesh::Point base, float delta, MyMesh::Point bound, std:
 MyMesh potential_field_mesh(MyMesh &mesh);
 
 void jacobi_laplace_smooth_and_expand(MyMesh &mesh, int smooth_times);
-
+}
 #endif
