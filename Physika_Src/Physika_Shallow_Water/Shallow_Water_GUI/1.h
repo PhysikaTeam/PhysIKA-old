950d0157c867d6658e9372ff763c43c88036df6f
superboxer <32252653+superboxer@users.noreply.github.com>
2019-05-24 23:56:03 +0800

    Update 1.h

--- Physika_Src/Physika_Shallow_Water/Shallow_Water_GUI/1.h
+++ Physika_Src/Physika_Shallow_Water/Shallow_Water_GUI/1.h
@@ -20,7 +20,7 @@ public:
 	std::vector<double> initial_velocity_z;
 	std::vector<double> initial_surface_level;
 	std::vector<double> initial_velocity;
-	SurfaceShow(size_t x_cells, size_t z_cells, float dt, float dx, int window_width, int window_height);
+	SurfaceShow(size_t x_cells, size_t z_cells, float dt, int window_width, int window_height);
 	~SurfaceShow();
 	void set_constants(bool m_have_tensor, float m_fric_coef, float m_gamma, float m_dt, float g, int situation=3, int times=0);
 	void init(std::string const surface, std::string const height, std::string const vx, std::string const vy, std::string const vz);
