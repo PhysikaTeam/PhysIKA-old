116b46516f9487738fcf1199d61985c8d89049e1
superboxer <32252653+superboxer@users.noreply.github.com>
2019-06-14 06:14:49 +0800

    Delete showmesh.h

--- Physika_Src/Physika_Surface_Fuild/Surface_Render/showmesh.h
+++
@@ -1,9 +0,0 @@
-#ifndef PHYSIKA_SURFACE_FUILD_SURFACE_RENDER_SHOWMESH_H_
-#define PHYSIKA_SURFACE_FUILD_SURFACE_RENDER_SHOWMESH_H_
-
-#include "Physika_Surface_Fuild/Surface_Triangle_Meshs/mymesh.h"
-namespace Physika{
-void show_mesh ( int argc, char *argv[]);
-void show_mesh(int argc, char *argv[], MyMesh const &mesh);
-}
-#endif
