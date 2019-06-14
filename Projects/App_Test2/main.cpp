c8526ff9920dfd0082c6ffef8d7ee07d57438bba
superboxer <32252653+superboxer@users.noreply.github.com>
2019-06-14 09:33:51 +0800

    Update main.cpp

--- Projects/App_Test2/main.cpp
+++ Projects/App_Test2/main.cpp
@@ -1,2 +1,7 @@
 #include "Physika_Shallow_Water/Shallow_Water_GUI/GLApp.h"
-
+int main(){
+  GLApp window(0.01,0.1,1200,900);
+  window.init("input/surface.txt","input/height.txt","input/velocity_x.txt","input/velocity_y.txt");
+  window.showframe();
+  return 0;
+}
