ec105891c56031ccaf8841031edff5967a6dce29
superboxer <32252653+superboxer@users.noreply.github.com>
2019-06-14 08:57:34 +0800

    Update load_txt_io2.cpp

--- Physika_Src/Physika_Surface_Fuild/Surface_IO/load_txt_io2.cpp
+++ Physika_Src/Physika_Surface_Fuild/Surface_IO/load_txt_io2.cpp
@@ -1,8 +1,10 @@
 #include "load_txt_io2.h"
+namespace Physika{
 void load_txt_io2(const std::string filename, std::vector<float> v) {
 	std::ifstream input(filename);
 	input.exceptions(std::istream::failbit | std::istream::badbit);
 	for (float &value : v) {
 		input >> value;
 	}
+}
 }
