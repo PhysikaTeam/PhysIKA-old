46839f0fbf15a3047242993dc464bafa6d92af3c
superboxer <32252653+superboxer@users.noreply.github.com>
2019-06-14 09:50:56 +0800

    Update main.cpp

--- Projects/App_Test3/main.cpp
+++ Projects/App_Test3/main.cpp
@@ -1,6 +1,6 @@
 #include "Physika_Surface_Fuild/Surface_GUI/GLApp.h"
 int main(){
-  GLApp window(101,101,0.5,1200,900);
+  GLApp window(100,100,0.5,1200,900);
   window.set_constants();
   window.init("input/surface.txt","input/height.txt","input/vx.txt","input/vy.txt","input/vz.txt");
   window.showframe(10);
