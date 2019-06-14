f3dd4fcc42fca4446e0f64e54abe78bba859c7cd
clouddon <clouddon@sina.com>
2019-06-14 20:04:50 +0800

    add instanced sphere shaders

--- Engine/Rendering/Shader/InstancedSphere.vs.glsl
+++ Engine/Rendering/Shader/InstancedSphere.vs.glsl
@@ -1,12 +1,24 @@
-#version 330 core
-layout (location = 0) in vec2 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aOffset;
+#version 330 compatibility
+layout (location = 0) in vec2 corner;
+layout (location = 2) in vec3 instance_pos;
+layout (location = 3) in vec3 instance_color;
 
-out vec3 fColor;
+uniform float sprite_size;
+
+out vec3 frag_color;
+out vec2 tex_coord;
+out vec3 frag_pos;
+out float out_sprite_size;
 
 void main()
 {
-    fColor = aColor;
-    gl_Position = vec4(aPos + aOffset, 0.0, 1.0);
+    vec3 posEye = vec3(gl_ModelViewMatrix * vec4(instance_pos, 1.0));
+
+    frag_pos = vec3(posEye.xy + corner * sprite_size, posEye.z);
+
+    frag_color = instance_color;
+    gl_Position = gl_ProjectionMatrix*vec4(frag_pos, 1.0);
+
+    tex_coord = corner;
+    out_sprite_size = sprite_size;
 }
