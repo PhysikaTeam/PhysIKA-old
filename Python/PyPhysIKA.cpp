bee87bb4bb7be137ad950a09e25d075b4862e836
clouddon <clouddon@sina.com>
2020-03-30 11:03:18 +0800

    Redirect the output directory for compilation on Ubuntu

--- Python/PyPhysIKA.cpp
+++ Python/PyPhysIKA.cpp
@@ -1,10 +1,10 @@
 #include "PyPhysIKA.h"
 
-//#include "PyGlutGUI.h"
+#include "PyGlutGUI.h"
 #include "PyCore.h"
-//#include "PyFramework.h"
-//#include "PyParticleSystem.h"
-//#include "PyRendering.h"
+#include "PyFramework.h"
+#include "PyParticleSystem.h"
+#include "PyRendering.h"
 
 // void init_GlutGUI(py::module &);
 // void init_Core(py::module &);
@@ -13,9 +13,9 @@ PYBIND11_MODULE(PyPhysIKA, m) {
 
 	m.doc() = "Python binding of PhysIKA";
 
-//	pybind_glut_gui(m);
+	pybind_glut_gui(m);
 	pybind_core(m);
-//	pybind_framework(m);
-//	pybind_particle_system(m);
-//	pybind_rendering(m);
+	pybind_framework(m);
+	pybind_particle_system(m);
+	pybind_rendering(m);
 }
