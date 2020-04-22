ea7e0549757c62b93b749d3fc8f9879794308e4f
clouddon <clouddon@sina.com>
2020-04-22 17:53:56 +0800

    Add support for find_package()

---
+++ Examples/App_QtGUI/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <memory>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
+#include "GUI/QtGUI/QtApp.h"
+
+#include "Framework/Framework/SceneGraph.h"
+#include "Dynamics/ParticleSystem/ParticleElasticBody.h"
+#include "Dynamics/ParticleSystem/StaticBoundary.h"
+#include "Dynamics/ParticleSystem/ElasticityModule.h"
+
+
+using namespace std;
+using namespace PhysIKA;
+
+
+int main()
+{
+	SceneGraph& scene = SceneGraph::getInstance();
+
+	std::shared_ptr<StaticBoundary<DataType3f>> root = scene.createNewScene<StaticBoundary<DataType3f>>();
+	root->loadCube(Vector3f(0), Vector3f(1), 0.005f, true);
+	root->setName("Root");
+
+// 	std::shared_ptr<ParticleElasticBody<DataType3f>> bunny = std::make_shared<ParticleElasticBody<DataType3f>>();
+// 	root->addParticleSystem(bunny);
+// //	bunny->getRenderModule()->setColor(Vector3f(0, 1, 1));
+// 	bunny->setMass(1.0);
+// 	bunny->loadParticles("../../Media/bunny/bunny_points.obj");
+// 	bunny->loadSurface("../../Media/bunny/bunny_mesh.obj");
+// 	bunny->translate(Vector3f(0.5, 0.2, 0.5));
+// 	bunny->setVisible(true);
+// //	bunny->getSurfaceRender()->setColor(Vector3f(1, 1, 0));
+// 	bunny->getElasticitySolver()->setIterationNumber(10);
+
+	QtApp window;
+	window.createWindow(1024, 768);
+
+	window.mainLoop();
+    SceneGraph::getInstance().setRootNode(nullptr);
+	return 0;
+}
