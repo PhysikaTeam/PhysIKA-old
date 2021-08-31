825820f91622bb4e500f2a8891c4c21d60e12d5f
LamKamhang <lamkamh@zju.edu.cn>
2021-08-31 13:43:42 +0800

    format predicates

--- Source/Dynamics/FiniteElementMethod/Tetrahedron/external/predicates/predicates.h
+++ Source/Dynamics/FiniteElementMethod/Tetrahedron/external/predicates/predicates.h
@@ -1,8 +1,8 @@
 #ifndef PREDICATES_FXZ_H
 #define PREDICATES_FXZ_H
 
-void exactinit();
-double orient2d(double const *pa, double const *pb, double const *pc);
-double orient3d(double const *pa, double const *pb, double const *pc, double const *pd);
+void   exactinit();
+double orient2d(double const* pa, double const* pb, double const* pc);
+double orient3d(double const* pa, double const* pb, double const* pc, double const* pd);
 
 #endif
